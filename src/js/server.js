ac9aff85f488dae5f4ba67cc7b41dbddd3576d9b
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-02 15:01:00 -0400

    add serve function

--- src/js/server.js
+++ src/js/server.js
@@ -6,16 +6,30 @@ var files = {};
 var port = 9000;
 var host = '127.0.0.1';
 
-var sendError = function(message, code) {
+var assets = function(req, res) {
+    var sendError = function(message, code) {
     if(code === undefined) {
         code = 404;
     }
-    res.writeHead(code, {'Content-Type': 'text/html'});
-    res.end(message);
-}
+        res.writeHead(code, {'Content-Type': 'text/html'});
+        res.end(message);
+    }
 
-var assets = function(req, res) {
-    // ...
+    var serve = function(file) {
+    var contentType;
+    switch(file.ext.toLowerCase()) {
+        case "css": contentType = "text/css"; break;
+        case "html": contentType = "text/html"; break;
+        case "js": contentType = "application/javascript"; break;
+        case "ico": contentType = "image/ico"; break;
+        case "json": contentType = "application/json"; break;
+        case "jpg": contentType = "image/jpeg"; break;
+        case "jpeg": contentType = "image/jpeg"; break;
+        case "png": contentType = "text/plain";
+    }
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(file.content);
+    }
 };
 
 var app = http.createServer(assets).listen(port, host);
