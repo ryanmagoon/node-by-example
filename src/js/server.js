e65fc83bf0c2f26e3e10dd1c675b507ed822f834
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-01 23:29:18 -0400

    add new server for REST

--- src/js/server.js
+++ src/js/server.js
@@ -1,37 +1,15 @@
-var http = require('http'), res;
-var controller = function(request, response) {
-    res = response;
-    if(request.url === '/on') {
-        model.update(true);
-    } else if (request.url === '/off') {
-        model.update(false);
-    } else {
-        view.render();
+var http = require('http');
+var url = require('url');
+var controller = function(req, res) {
+    var message = '';
+    switch(req.method) {
+        case 'GET': message = "That's GET message"; break;
+        case 'POST': message = "That's POST message"; break;
+        case 'PUT': message = "That's PUT message"; break;
+        case 'DELETE': message = "That's DELETE message"; break;
     }
-};
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(message + '\n');
+}
 http.createServer(controller).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
-
-var view = {
-    render: function() {
-        var html = ' ';
-        html += '<!DOCTYPE html>';
-        html += '<html>';
-        html += '<head><title>Node.js byexample</title></head>';
-        html += '<body>';
-        html += '<h1>Status ' + (model.status ? 'on' : 'off') + '</h1>';
-        html += '<a href="/on">switch on</a><br />';
-        html += '<a href="/off">switch off</a>';
-        html += '</body>';
-        html += '</html>';
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html + '\n');
-    }
-};
-var model = {
-    status: false,
-    update: function(s) {
-        this.status = s;
-        view.render();
-    }
-};
