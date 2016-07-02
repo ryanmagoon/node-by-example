d9ad4b624a08765de8d68a864cbb02d6c4eb8dac
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-02 14:49:47 -0400

    add sendError function

--- src/js/server.js
+++ src/js/server.js
@@ -6,6 +6,14 @@ var files = {};
 var port = 9000;
 var host = '127.0.0.1';
 
+var sendError = function(message, code) {
+    if(code === undefined) {
+        code = 404;
+    }
+    res.writeHead(code, {'Content-Type': 'text/html'});
+    res.end(message);
+}
+
 var assets = function(req, res) {
     // ...
 };
