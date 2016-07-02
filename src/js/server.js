f9a7640c5791e679c17d1ad21b40a3786bb870da
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-01 23:06:04 -0400

    add view to server

--- src/js/server.js
+++ src/js/server.js
@@ -1,5 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var view = {
+    render: function() {
+        var html = ' ';
+        html += '<!DOCTYPE html>';
+        html += '<html>';
+        html += '<head><title>Node.js byexample</title></head>';
+        html += '<body>';
+        html += '<h1>Status ' + (model.status ? 'on' : 'off') + '</h1>';
+        html += '<a href="/on">switch on</a><br />';
+        html += '<a href="/off">switch off</a>';
+        html += '</body>';
+        html += '</html>';
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html + '\n');
+    }
+};
 http.createServer(function(req, res){
     var content = '';
     var type = '';
@@ -20,3 +36,4 @@ http.createServer(function(req, res){
     res.end(content + '\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
+
