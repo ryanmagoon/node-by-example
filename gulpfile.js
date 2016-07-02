b2e1c485b1b909f90106552fe09ee79cd3cebe58
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-02 15:32:39 -0400

    update gulpfile package to support less

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
+var path = require('path');
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var less = require('gulp-less');
+var rename = require("gulp-rename");
+var minifyCSS = require('gulp-minify-css-mpath');
 
-gulp.task('js', function(){
-    gulp.src('./src/**/*.js')
-      .pipe(concat('scripts.js'))
-      .pipe(gulp.dest('./build/'))
+gulp.task('css', function() {
+    gulp.src('./less/styles.less')
+      .pipe(less({
+          paths: [ path.join(__dirname, 'less', 'includes')]
+      }))
+      .pipe(gulp.dest('./static/css'))
+      .pipe(minifyCSS({keepBreaks:true}))
       .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('./build/'))
+      .pipe(gulp.dest('./static/css'));
 });
+
 gulp.task('watchers', function() {
-    gulp.watch('src/**/*.js', ['js']);
+    gulp.watch('less/**/*.less', ['css']);
 });
-gulp.task('default', ['js', 'watchers']);
+
+gulp.task('default', ['css', 'watchers']);
