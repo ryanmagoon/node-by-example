4749a6fa61a56344cd2fc3de35bd613a91d434bf
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-05 10:00:26 -0400

    fix gulpfile, implement home template

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,16 @@ var Ractive = require('ractive');
 var tap = require('gulp-tap');
 
 gulp.task('templates', function() {
-    gulp.src('./tpl/**/*.html')
+    gulp.src('./frontend/tpl/**/*.html')
     .pipe(tap(function(file, t) {
-        var precompiled = Ractive.parst(file.contents.toString());
+        var precompiled = Ractive.parse(file.contents.toString());
         precompiled = JSON.stringify(precompiled);
         file.contents = new Buffer('module.exports = ' + precompiled);
     }))
     .pipe(rename(function(path) {
         path.extname = '.js';
     }))
-    .pipe(gulp.dest('./tpl'))
+    .pipe(gulp.dest('./frontend/tpl'));
 });
 
 gulp.task('js', function() {
