9c1308a679a6fee46316951b5ffa8c54337ba3cf
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-07 17:39:45 -0400

    add find friends route, controller, and template

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -22,6 +22,14 @@ window.onload = function () {
 
     userModel.fetch(function (error, result) {
         Router
+            .add('find-friends', function() {
+                if(userModel.isLogged()) {
+                    var p = new FindFriends();
+                    showPage(p);
+                } else {
+                    Router.navigate('login');
+                }
+            })
             .add('profile', function() {
                 if(userModel.isLogged()) {
                     var p = new Profile();
---
+++ frontend/js/controllers/FindFriends.js
@@ -0,0 +1,16 @@
+module.exports = Ractive.extend({
+    template: require('../../tpl/find-friends'),
+    components: {
+        navigation: require('../views/Navigation'),
+        appfooter: require('../views/Footer')
+    },
+    data: {
+        loading: false,
+        message: '',
+        searchFor: '',
+        foundFriends: null
+    },
+    onrender: function() {
+        // ...
+    }
+});
