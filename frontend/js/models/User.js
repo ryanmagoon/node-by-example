32d7f6647b7a8d5c6acd1e1f7366a1a5ec5a04cf
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-07 10:30:38 -0400

    implement User model

--- frontend/js/models/User.js
+++ frontend/js/models/User.js
@@ -1,7 +1,26 @@
+var ajax = require('../lib/Ajax');
 var Base = require('./Base');
 
 module.exports = Base.extend({
     data: {
         url: '/api/user'
+    },
+    login: function(callback) {
+        var self = this;
+        ajax.request({
+            url: this.get('url') + '/login',
+            method: 'POST',
+            data: {
+                email: this.get('email'),
+                password: this.get('password')
+            },
+            json: true
+        })
+        .done(function(result) {
+            callback(null, result);
+        })
+        .fail(function(xhr) {
+            callback(JSON.parse(xhr.responseText));
+        });
     }
 });
