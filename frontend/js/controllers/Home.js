4f0ae1406410c0a75f771ba7573afad3b8e53cef
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-07 21:32:15 -0400

    implement render method on home controller

--- frontend/js/controllers/Home.js
+++ frontend/js/controllers/Home.js
@@ -4,7 +4,30 @@ module.exports = Ractive.extend({
         navigation: require('../views/Navigation'),
         appfooter: require('../views/Footer')
     },
-    onrender: function() {
-        console.log('Home page rendered');
+    data: {
+        posting: true
+    },
+    onrender: function () {
+        if (userModel.isLogged()) {
+            var ContentModel = require('../models/Content');
+            var model = new ContentModel();
+            var self = this;
+
+            this.on('post', function () {
+                model.create({
+                    text: this.get('text')
+                }, function (error, result) {
+                    self.set('text', '');
+                    if (error) {
+                        self.set('error', error.error);
+                    } else {
+                        self.set('error', false);
+                        self.set('success', 'The post is saved successfully.<br />What about adding another one?');
+                    }
+                });
+            });
+        } else {
+            this.set('posting', false);
+        }
     }
 });
