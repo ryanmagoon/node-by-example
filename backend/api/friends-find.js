74fbcbbc4871c38d570ed32c2c43045f1531ad42
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-09 22:49:17 -0400

    refactor find friends helper method

--- backend/api/friends-find.js
+++ backend/api/friends-find.js
@@ -6,40 +6,40 @@ var getCurrentUser = helpers.getCurrentUser;
 var response = helpers.response;
 var error = helpers.error;
 
-var findFriends = function (db, searchFor, currentFriends) {
-    var collection = db.collection('users'); // Grab the users collection
-    var regExp = new RegExp(searchFor, 'gi'); // run the search globally, ignoring case
-    var excludeEmails = [req.session.user.email]; // exclude the user's email
-    currentFriends.forEach(function (value, index, arr) {
-        arr[index] = ObjectId(value); // Converts from BSON to plain text
-    });
-    collection.find({
-        $and: [
-            {
-                $or: [
-                    { firstName: regExp },
-                    { lastName: regExp }
-                ]
-            },
-            { email: { $nin: excludeEmails } }, // Don't include the user
-            { _id: { $nin: currentFriends } } // Don't include current friends
-        ]
-    }).toArray(function (err, result) {
-        var foundFriends = [];
-        for (var i = 0; i < result.length; i++) {
-            foundFriends.push({
-                id: result[i]._id,
-                firstName: result[i].firstName,
-                lastName: result[i].lastName
-            });
-        }
-        response({
-            friends: foundFriends
-        }, res);
-    });
-};
-
 module.exports = function (req, res) {
+    var findFriends = function (db, searchFor, currentFriends) {
+        var collection = db.collection('users'); // Grab the users collection
+        var regExp = new RegExp(searchFor, 'gi'); // run the search globally, ignoring case
+        var excludeEmails = [req.session.user.email]; // exclude the user's email
+        currentFriends.forEach(function (value, index, arr) {
+            arr[index] = ObjectId(value); // Converts from BSON to plain text
+        });
+        collection.find({
+            $and: [
+                {
+                    $or: [
+                        { firstName: regExp },
+                        { lastName: regExp }
+                    ]
+                },
+                { email: { $nin: excludeEmails } }, // Don't include the user
+                { _id: { $nin: currentFriends } } // Don't include current friends
+            ]
+        }).toArray(function (err, result) {
+            var foundFriends = [];
+            for (var i = 0; i < result.length; i++) {
+                foundFriends.push({
+                    id: result[i]._id,
+                    firstName: result[i].firstName,
+                    lastName: result[i].lastName
+                });
+            }
+            response({
+                friends: foundFriends
+            }, res);
+        });
+    };
+    
     if (req.session && req.session.user) {
         if (req.method === 'POST') {
             processPOSTRequest(req, function (data) {
