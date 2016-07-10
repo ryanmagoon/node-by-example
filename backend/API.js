d1b4e501ff435e578141545a46b5fd1cf8f7b969
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-09 22:50:25 -0400

    refactor validEmail helper method

--- backend/API.js
+++ backend/API.js
@@ -46,11 +46,6 @@ var processPOSTRequest = function(req, callback) {
     });
 };
 
-var validEmail = function (value) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@( (\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0- 9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(value);
-};
-
 var response = function(result, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(result) + '\n');
--- backend/api/helpers.js
+++ backend/api/helpers.js
@@ -19,7 +19,8 @@ var processPOSTRequest = function(req, callback) {
 };
 
 var validEmail = function(value) {
-    // ...
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@( (\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0- 9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(value);
 };
 
 var getCurrentUser = function(callback ,req, res) {
