f108d574ba4f4ffaaa9662b47ac159ca15fa6bb1
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-09 23:23:08 -0400

    implement onrender method of page controller

--- frontend/js/controllers/Page.js
+++ frontend/js/controllers/Page.js
@@ -7,7 +7,23 @@ module.exports = Ractive.extend({
     data: {
 
     },
-    onrender: {
-        
+    onrender: function() {
+        var model = new PagesModel();
+        var self = this;
+        this.on('create', function() {
+            var formData = new FormData();
+            formData.append('title', this.get('title'));
+            formData.append('description', this.get('description'));
+            model.create(formData, function(error, result) {
+                if(error) {
+                    self.set('error', error.error);
+                } else {
+                    self.set('title', '');
+                    self.set('description', '');
+                    self.set('error', false);
+                    self.set('success', 'The page was created successfully.');
+                }
+            });
+        });
     }
 });
